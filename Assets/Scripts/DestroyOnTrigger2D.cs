2951328f982665abb72f8d71b5a953068be2ad3a
Eli Levi <el1l3vi@gmail.com>
2023-04-13 21:06:19 +0300

    Made tetris car racing

---
+++ Assets/Scripts/DestroyOnTrigger2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class DestroyOnTrigger2D : MonoBehaviour
+{
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string dmgTag;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == dmgTag)
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
